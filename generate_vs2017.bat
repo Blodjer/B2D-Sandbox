c7ba634763ecbbd5ed580a64cdc8f769edcb8a31
Blodjer <j95.szwaczka@gmail.com>
2019-02-17 15:35:19 +0100

    Premake setup

---
+++ generate_vs2017.bat
@@ -0,0 +1,4 @@
+"B2D/ThirdParty/premake5.exe" vs2017
+
+@echo off
+if errorlevel 1 pause
---
+++ premake5.lua
@@ -0,0 +1,62 @@
+workspace "B2D-Sandbox"
+	cppdialect "C++17"
+	systemversion "latest"
+
+	startproject "Sandbox"
+
+	targetdir "Bin/%{cfg.buildcfg}/%{cfg.system}-%{cfg.architecture}"
+	objdir "Tmp/%{cfg.buildcfg}/%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+	platforms
+	{
+		"Win32",
+		"Win64"
+	}
+	
+	filter "configurations:Debug"
+		symbols "On"
+		optimize "Off"
+
+	filter "configurations:Release"
+		symbols "Off"
+		optimize "On"
+
+	filter "platforms:Win32"
+		system "Windows"
+		architecture "x86"
+
+	filter "platforms:Win64"
+		system "Windows"
+		architecture "x64"
+
+	include "B2D"
+
+project "Sandbox"
+	location "Sandbox"
+	language "C++"
+	kind "ConsoleApp"
+
+	files
+	{
+		"%{prj.location}/Source/**"
+	}
+
+	includedirs
+	{
+		"B2D/Source/",
+		"B2D/Include/"
+	}
+
+	links
+	{
+		"B2D"
+	}
+
+	filter "system:windows"
+		buildoptions ("/sdl")
