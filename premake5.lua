4157ee277caefb99a655e6487e2eea698600fa54
Blodjer <j95.szwaczka@gmail.com>
2019-02-26 00:31:46 +0100

    Updated premake setup

--- premake5.lua
+++ premake5.lua
@@ -1,42 +1,5 @@
 workspace "B2D-Sandbox"
-	cppdialect "C++17"
-	systemversion "latest"
-
 	startproject "Sandbox"
-
-	targetdir "Bin/%{cfg.buildcfg}/%{cfg.system}-%{cfg.architecture}"
-	objdir "Tmp/%{cfg.buildcfg}/%{cfg.system}-%{cfg.architecture}/%{prj.name}"
-
-	flags { "MultiProcessorCompile" }
-
-	configurations
-	{
-		"Debug",
-		"Release"
-	}
-
-	platforms
-	{
-		"Win32",
-		"Win64"
-	}
-	
-	filter "configurations:Debug"
-		symbols "On"
-		optimize "Off"
-
-	filter "configurations:Release"
-		symbols "Off"
-		optimize "On"
-
-	filter "platforms:Win32"
-		system "Windows"
-		architecture "x86"
-
-	filter "platforms:Win64"
-		system "Windows"
-		architecture "x64"
-	
 	include "B2D"
 
 project "Sandbox"
