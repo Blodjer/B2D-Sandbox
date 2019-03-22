1f6709e3fdc06e328be38d82a6734eb5ee41e639
Blodjer <j95.szwaczka@gmail.com>
2019-03-22 02:53:22 +0100

    sync

--- Sandbox/Source/Main.cpp
+++ Sandbox/Source/Main.cpp
@@ -1,38 +1,40 @@
 #include "EntryPoint.h"
 
-#include "Component/BoxCollider.h"
-#include "GameObject.h"
-#include "Level.h"
-#include "Player.h"
+#include "ECS/World.h"
+#include "ECS/Component/SpriteComponent.h"
+#include "ECS/Component/TransformComponent.h"
+#include "ECS/Component/HoverComponent.h"
 
 void B2D::Config(ApplicationConfig& config)
 {
 	config.name = "Sandbox";
 }
 
-void B2D::PopulateLevel(CLevel* const level)
+void B2D::PopulateWorld(World* const world)
 {
-    Player* player = level->SpawnGameObject<Player>();
-
-    CGameObject* gameObject = level->SpawnGameObject();
-    CBoxCollider* collider = gameObject->AddComponent<CBoxCollider>();
-    collider->SetSize(600, 100);
-
-    /*
     auto start = std::chrono::high_resolution_clock::now();
 
-    int m = 20;
+    int m = 400;
+
+    //mWorld->AddSystemEntityObject<EcsPlayer>();
+
     for (int i = 0; i < m; i++)
     {
-        Player* g = level->SpawnGameObject<Player>();
+        EntityID entity = world->AddEntity<Entity>();
 
-        for (CComponent* const c : g->GetComponents())
-        {
-            c->SetRelativePosition(TVec2(UMath::RandomRange(-400, 400), UMath::RandomRange(-200, 200)));
-        }
+        ResourcePtr<CTexture> textures = IResourceManager::Get<CTexture>("Content/Sprites/Character.png");
+        SpriteComponent* s = world->AddComponent<SpriteComponent>(entity, CShader::Load("Content/Shader/DefaultVS.glsl", "Content/Shader/SimpleSpritePS.glsl"));
+        s->material.SetTexture(0, textures);
+
+        TransformComponent* t = world->AddComponent<TransformComponent>(entity);
+        t->position = TVec3(UMath::RandomRange(-5.0f, 5.0f), UMath::RandomRange(-3.0f, 3.0f), UMath::RandomRange(-1.0f, 1.0f));
+        t->matrix = TMatrix::Translate(t->matrix, t->position);
+        t->matrix = TMatrix::Scale(t->matrix, t->scale);
+
+        HoverComponent* h = world->AddComponent<HoverComponent>(entity);
+        h->speed = UMath::RandomRange(1.0f, 5.0f);
     }
 
     std::chrono::duration<double> elapsed = std::chrono::high_resolution_clock::now() - start;
-    B2D_INFO("Loading of {0} GameObjects took {1}s", m, elapsed.count());
-    */
+    B2D_INFO("Loading of {0} Entities took {1}s", m, elapsed.count());
 }
